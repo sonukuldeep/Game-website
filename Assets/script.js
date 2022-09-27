b62aa66666ff070dcd9ed4e1d0584d6660a98844
sonukuldeep <sonukuldeep@gmail.com>
2022-09-27 20:42:33 +0530

    added animated cat

--- Assets/script.js
+++ Assets/script.js
@@ -10,3 +10,32 @@ function Activate(target) {
     removeTag()
     target.classList.add('active')
 }
+
+//cat animation
+const eyes = document.querySelectorAll(".eyes");
+const anchor = document.querySelector("#anchor");
+const rekt = anchor.getBoundingClientRect();
+const anchorX = rekt.left + rekt.width / 2;
+const anchorY = rekt.top + rekt.height / 2;
+
+// main calculation
+const angle = (cx, cy, ex = anchorX, ey = anchorY) => {
+  const dy = ey - cy;
+  const dx = ex - cx;
+  const rad = Math.atan2(dy, dx);
+  const deg = (rad * 180) / Math.PI;
+  return deg;
+}
+
+// event listner that listens to cursor and moves the eye accordingly
+document.addEventListener("mousemove", (e) => {
+  let mouseX = e.clientX;
+  let mouseY = e.clientY;
+  let angleInDeg;
+
+  angleInDeg = angle(mouseX, mouseY);
+  eyes.forEach((eye) => {
+    eye.style.transform = `rotate(${angleInDeg}deg)`;
+    anchor.style.filter = `hue-rotate(${angleInDeg}deg)`;
+  });
+});
