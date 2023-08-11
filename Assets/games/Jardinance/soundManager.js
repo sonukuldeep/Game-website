e1ae6e0c56738c519ec4e5083d7f4aef50487000
sonukuldeep <sonukuldeep@outlook.com>
2023-08-11 21:21:44 +0530

    link fix

--- Assets/games/Jardinance/soundManager.js
+++ Assets/games/Jardinance/soundManager.js
@@ -1,12 +1,12 @@
 const soundsArray = [
-    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/bubble1.wav",
-    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/bubble2.wav",
-    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/bubble3.wav",
-    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/ball-on-platform1.wav",
-    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/ball-on-platform2.wav",
-    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/explosion1.wav",
-    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/explosion2.wav",
-    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/explosion3.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/sound_effects/bubble1.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/sound_effects/bubble2.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/sound_effects/bubble3.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/sound_effects/ball-on-platform1.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/sound_effects/ball-on-platform2.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/sound_effects/explosion1.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/sound_effects/explosion2.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/sound_effects/explosion3.wav",
 ];
 export const soundsArrayLength = soundsArray.length;
 export function SoundEffect(sound) {
