72af85868b5457b951de6525b531d49d63be29e4
sonukuldeep <sonukuldeep@outlook.com>
2023-08-11 21:07:37 +0530

    sound

--- Assets/games/Jardinance/soundManager.js
+++ Assets/games/Jardinance/soundManager.js
@@ -1,12 +1,12 @@
 const soundsArray = [
-    "/assets/sound effects/bubble1.wav",
-    "/assets/sound effects/bubble2.wav",
-    "/assets/sound effects/bubble3.wav",
-    "/assets/sound effects/ball-on-platform1.wav",
-    "/assets/sound effects/ball-on-platform2.wav",
-    "/assets/sound effects/explosion1.wav",
-    "/assets/sound effects/explosion2.wav",
-    "/assets/sound effects/explosion3.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/bubble1.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/bubble2.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/bubble3.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/ball-on-platform1.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/ball-on-platform2.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/explosion1.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/explosion2.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/explosion3.wav",
 ];
 export const soundsArrayLength = soundsArray.length;
 export function SoundEffect(sound) {
