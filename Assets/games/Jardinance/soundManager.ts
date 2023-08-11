6079ed0cf55c72e459d4a3cbe24ec50c8a028f0b
sonukuldeep <sonukuldeep@outlook.com>
2023-08-11 21:11:27 +0530

    sound fix

--- Assets/games/Jardinance/soundManager.ts
+++ Assets/games/Jardinance/soundManager.ts
@@ -10,14 +10,14 @@
 // ]
 
 const soundsArray: string[] = [
-    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/bubble1.wav",
-    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/bubble2.wav",
-    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/bubble3.wav",
-    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/ball-on-platform1.wav",
-    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/ball-on-platform2.wav",
-    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/explosion1.wav",
-    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/explosion2.wav",
-    "https://weirdgames.netlify.app/assets/games/jardinance/assets/images/characters.png/assets/sound_effects/explosion3.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/sound_effects/bubble1.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/sound_effects/bubble2.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/sound_effects/bubble3.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/sound_effects/ball-on-platform1.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/sound_effects/ball-on-platform2.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/sound_effects/explosion1.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/sound_effects/explosion2.wav",
+    "https://weirdgames.netlify.app/assets/games/jardinance/assets/sound_effects/explosion3.wav",
 ]
 export const soundsArrayLength = soundsArray.length
 
